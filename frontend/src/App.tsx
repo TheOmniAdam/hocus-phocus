1be8e372df61367aa30b3c9a1def15f984674e4a
Adam M. Glazier <AdamMGlazier@Gmail.com>
2026-09-20 06:11:20 -0700

    Add initial activity card component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ActivityCard from './components/ActivityCard'
+
+function App() {
+  return (
+    <>
+      <h1>Hocus PHocus</h1>
+      <p>
+        Your life is a resource management game.
+      </p>
+      
+      <ActivityCard />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/ActivityCard.tsx
@@ -0,0 +1,16 @@
+function ActivityCard() {
+    return(
+        <>
+            <p>
+                Apply to Acme Corp 
+            </p>
+            <p>
+                Career
+            </p>
+            <p>
+                45 minutes
+            </p>
+        </>
+    )
+}
+export default ActivityCard
